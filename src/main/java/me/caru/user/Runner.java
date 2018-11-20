20158b1d50a37e6b1b327f050460457dac9917b1
조경대 <kyungdae.cho@navercorp.com>
2018-11-20 16:40:29 +0900

    first commit

---
+++ src/main/java/me/caru/user/Runner.java
@@ -0,0 +1,29 @@
+package me.caru.user;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Runner
+ *
+ * @author kyungdae.cho
+ * @version 1.0.0
+ * @since 2018. 11. 20.
+ */
+@Component
+public class Runner implements ApplicationRunner {
+
+	@Value("${a}")
+	private String a;
+
+	@Value("${spring.profiles.active}")
+	private String profile;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("a: " + a);
+		System.out.println("profile: " + profile);
+	}
+}
